a458e1d4c69b599a1d83e8d4123faaaf31359378
Gianluca <37697178+Trusted97@users.noreply.github.com>
2019-08-22 10:07:17 +0200

    Update GTMSPider - Format File
    ToDo : Translate Comment [X]
    ToDo : Get only head tag []
    
    Now Spider is a function

--- gtmspider.py
+++ gtmspider.py
@@ -4,39 +4,54 @@
 #https://www.gianlucabenucci.com/#portfolio
 
 #pyppeteer.errors.NetworkError: Protocol error Page.navigate: Target closed. Errore causato da vecchia versione di WebSocket
-# Fix: Da terminale pip3 install websockets==6.0 --force-reinstall
+# Fix: From terminal pip3 install websockets==6.0 --force-reinstall
 
-#Try-Except per evitare che sollevi errori se non sono stati installati i moduli necessari
+#ToDo Get Only the <head> tag from rendered page?
 
-#ToDo Translate comments
+#Try-Except for avoid crash/error if modules are not avaible
 try:
     from googlesearch import search
     from requests_html import HTMLSession
 except ImportError:  
-    print("Installa tutti i moduli per avviarlo")
+    print("Install all modules for use it")
+
+
+def GTMSpider(query):
+    session = HTMLSession() # Initialize HTMLSession
+
+    #query : query string that we want to search for.
+    #tld : tld stands for top level domain which means we want to search our result on google.com or google.in or some other domain.
+    #lang : lang stands for language.
+    #num : Number of results we want.
+    #start : First result to retrieve.
+    #stop : Last result to retrieve. Use None to keep searching forever.
+    #pause : Lapse to wait between HTTP requests. Lapse too short may cause Google to block your IP. Keeping significant lapse will make your program slow but its safe and better option.
+    #Return : Generator (iterator) that yields found URLs. If the stop parameter is None the iterator will loop forever.
+
+    for current_url in search(query, lang="en", num=10, start=0, stop=10, pause=15, only_standard=True):
+
+        try:
+            
+            content_js = session.get(current_url) #Open session for current url
+            content_js.html.render() #Render page
+
+            rendered_html = content_js.html.html #Contenuto della pagina con js renderizzato 
+
+            check_GTM_URL = rendered_html.find("www.googletagmanager.com/gtm.js") #Check for see if GTM url is in page
+            check_GTM_ACRONYM = rendered_html.find("GTM-") #Check for see if GTM Acronym is in page
+            
+
+            if ( check_GTM_URL > 0 or check_GTM_ACRONYM > 0):
+                print ("Google Tag Manager currently in use! " + current_url)
+            else:
+                print ("Google Tag Manager not in use! " + current_url)
+                
+        except:
+            print ("Error while analyze this url: " + current_url)
   
-#query da cercare 
-query = "search term here"
+#query to search on Google 
+query = input("Insert query: ")
 
-session = HTMLSession() # Inizializzo HTMLSession
-  
-for current_url in search(query, lang="it", num=10, start=0, stop=10, pause=15, only_standard=True):
+print("Start Scraping...")
 
-    try:
-        content_js = session.get(current_url) #Apro la sessione per url corrente
-        content_js.html.render() #Renderizzo la pagina
-
-        rendered_html = content_js.html.html #Contenuto della pagina con js renderizzato
-
-        check_GTM_URL = rendered_html.find("www.googletagmanager.com/gtm.js") #Check utilizzati per vedere se GTM è presente
-        check_GTM_ACRONYM = rendered_html.find("GTM-") #Check utilizzati per vedere se GTM è presente
-
-        #print (check_GTM_URL)
-        #print (check_GTM_ACRONYM)
-
-        if ( check_GTM_URL > 0 or check_GTM_ACRONYM > 0):
-            print ("Google Tag Manager in uso! " + current_url)
-        else:
-            print ("Google Tag Manager non in uso! " + current_url)
-    except:
-        print ("Errore in esecuzione: " + current_url)
+GTMSpider(query)
